db53860d0286e2be8007df6d3e306f8687c5813e
ahura-del <anuredel@gmail.com>
2022-04-18 16:58:09 +0430

    create validation file

---
+++ Validation.js
@@ -0,0 +1,22 @@
+const Joi = require('joi')
+
+const registerValidation = data=>{
+    const schema = Joi.object({
+        name:Joi.string().min(2).required(),
+        lastName:Joi.string().min(2).required(),
+        email:Joi.string().required().email(),
+        password:Joi.string().min(4).max(1024).required()
+    })
+    return schema.validate(data)
+}
+
+const loginValidation = data =>{
+    const schema = Joi.object({
+        email:Joi.string().required().email(),
+        password:Joi.string().min(4).max(1024).required()
+    })
+    return schema.validate(data)
+}
+
+module.exports.registerValidation = registerValidation;
+module.exports.loginValidation = loginValidation
