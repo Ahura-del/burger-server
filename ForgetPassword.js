365fab538c5641aa8c0ec7486a234c4bec59e829
ahura-del <anuredel@gmail.com>
2022-04-19 17:11:36 +0430

    create forget password email structure

---
+++ ForgetPassword.js
@@ -0,0 +1,32 @@
+const nodemailer = require('nodemailer')
+const env = require("dotenv")
+env.config()
+const transport = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.PASS,
+  },
+})
+
+const sendForgetPassword = async (user , code)=>{
+    let mailOption = {
+        from: "Burger@example.com",
+        to: user.email,
+        subject: "Burger app forget password code",
+        html: `
+                <p>Dear ${user.name} ${user.lastName}</p>
+                <p>You'r reset password code is :</p>
+                <h2>${code}</h2>
+                <br/>
+                <br/>
+                <small>Burger app</small>
+            `,
+      };
+      return await transport.sendMail(mailOption);
+}
+
+module.exports = sendForgetPassword
