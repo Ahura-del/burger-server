cd517e4ee7d88f71bd1a9b0c94440577c5e6f285
ahura-del <ahuradel@gmail.com>
2022-04-27 19:17:18 +0430

    set mongodb variable in env file

--- server.js
+++ server.js
@@ -5,13 +5,12 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const env = require("dotenv").config();
 const authRoute = require('./Routes/Auth')
-const productsRoute = require('./Routes/Products')
-
+const PORT = process.env.PORT || 3000
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: false }));
 
-mongoose.connect('mongodb://localhost:27017',{
+mongoose.connect(process.env.MONGODB,{
     useNewUrlParser:true,
     useUnifiedTopology:true
   } ,() => {
@@ -21,6 +20,5 @@ mongoose.connect('mongodb://localhost:27017',{
     res.send("Burger API")
   })
 app.use('/auth' , authRoute)
-app.use('/products' , productsRoute )
 
-app.listen(3000 , ()=> console.log('connect'))
+app.listen(PORT , ()=> console.log('connect'))
